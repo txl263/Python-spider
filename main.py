ecab1019ef651fb65cfacb027c756041acc80240
txl263 <xiaolei.tien@gmail.com>
2017-05-16 01:17:20 +0800

    update

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import time
 from job_model import Job
 import peewee
 import sys
-city_name="beijing"
+op = Options()
+op.add_argument('user-data-dir=/Volumes/MAC_DATA_USB/Eric/Library/Application Support/Google/Chrome/Default')
+
 class JobSpider:
-    def __int__(self):
-        op = Options()
-        op.add_argument('user-data-dir=/Volumes/MAC_DATA_USB/Eric/Library/Application Support/Google/Chrome/Default')
-        #dr = self.driver
+    def __init__(self):
+        
+        #driver = self.driver
         self.driver = webdriver.Chrome(chrome_options=op)
         #self.city_map = {}
         #self.driver = webdriver.chrome()
-        self.driver.implicitly_wait(30)
+        #self.driver.implicitly_wait(30)
         self.baseUrl = 'http://www.51job.com/'
         self.driver.get(self.baseUrl)
         time.sleep(0.5)
-        city_name = 'beijing'
-        self.city_name='beijing'
-        driver.find_element_by_xpath("//input[@id='kwdselectid']").send_keys("软件测试工程师")
+        self.driver.find_element_by_xpath("//input[@id='kwdselectid']").send_keys("软件测试工程师")
+        self.driver.find_element_by_xpath("//*[@id='work_position_click']").click()
+        self.driver.find_element_by_xpath('//*[@id="work_position_click_multiple_selected_each_010000"]').click()
+        self.driver.find_element_by_xpath('//*[@id="work_position_click_center_right_list_category_000000_010000"]').click()
+        self.driver.find_element_by_xpath('//*[@id="work_position_click_bottom_save"]').click()
+        self.driver.find_element_by_xpath("//button[contains(.,'搜索')]").click()
 
     def search(self,keyword=u'软件测试工程师'):
         #打开首页，输入搜索内容，然后点击按钮
@@ -55,8 +60,9 @@ class JobSpider:
         return not disabled
 
     def get_end_page(self):
-        self.end_page = int(self.dr.find_element_by_xpath('.totalNum').text)
-        print (self.end_page)
+        self.end_page = self.driver.find_element_by_xpath("//*[@id='resultList']/div[1]/div[4]").text
+        self.end_page = self.end_page[-2:]
+        print self.end_page
         return self.end_page
 
     def goto_netxt_page(self):
@@ -98,10 +104,10 @@ class JobSpider:
             print 'position %s, id = %s, already exists' %(position_name,position_id)
 
 
-    def start(self, city_name):
+    def start(self):
         self.search(u'测试')
-        self.select_city(city_name)
-        print city_name
+        # self.select_city(city_name)
+        # print city_name
         end_page = self.get_end_page()
         self.get_job_info_per_page()
         while sef.has_next_page:
@@ -118,5 +124,5 @@ class JobSpider:
 
 if __name__=="__main__":
     spider = JobSpider()
-    spider.start(city_name)
+    spider.start()
     spider.end()
